d2b880685c3a582c76b163bd785250f9da7374b8
Nekomercat <l.a.b.tssj4@gmail.com>
2021-11-17 20:32:23 -0600

    Harvest vespene gas

--- protoss_agent.py
+++ protoss_agent.py
@@ -6,8 +6,80 @@ import random
 
 class ProtossAgent(base_agent.BaseAgent):
 
+    def __init__(self):
+        super(ProtossAgent, self).__init__()
+        self.attack_coordinates= None
+
+    def unit_type_is_selected(self, obs, unit_type):
+        if (len(obs.observation.single_select) > 0 and obs.observation.single_select[0].unit_type == unit_type):
+            return True
+
+        if (len(obs.observation.multi_select) > 0 and obs.observation.multi_select[0].unit_type == unit_type):
+            return True
+
+        return False
+
+    def get_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.feature_units if unit.unit_type == unit_type]
+
+    def can_do(self, obs, action):
+        return action in obs.observation.available_actions
+
+    def build_assimilator(self, obs):
+        neutral_vespene_geysers = self.get_units_by_type(obs, units.Neutral.VespeneGeyser)
+        assimilators = self.get_units_by_type(obs, units.Protoss.Assimilator)
+
+        if len(assimilators) < 1 and len(neutral_vespene_geysers) > 0:
+            if self.unit_type_is_selected(obs, units.Protoss.Probe):
+                geyser = random.choice(neutral_vespene_geysers)
+                return actions.FUNCTIONS.Build_Assimilator_screen("now", (geyser.x, geyser.y))
+
+            probes = self.get_units_by_type(obs, units.Protoss.Probe)
+
+            if len(probes) > 0:
+                probes = random.choice(probes)
+                return actions.FUNCTIONS.select_point("select_all_type", (probes.x, probes.y))
+
+    def gather_vespene_gas(self, obs):
+        assimilator = self.get_units_by_type(obs, units.Protoss.Assimilator)
+        if len(assimilator) > 0:
+            assimilator = random.choice(assimilator)
+            if assimilator['assigned_harvesters'] < 3:
+                if self.unit_type_is_selected(obs, units.Protoss.Probe):
+                    if len(obs.observation.single_select) < 2 and len(obs.observation.multi_select) < 2:
+                        if self.can_do(obs, actions.FUNCTIONS.Harvest_Gather_screen.id):
+                            return actions.FUNCTIONS.Harvest_Gather_screen("now", (assimilator.x, assimilator.y))
+
+
+                probes = self.get_units_by_type(obs, units.Protoss.Probe)
+                if len(probes) > 0:
+                    probe = random.choice(probes)
+                    return actions.FUNCTIONS.select_point("select", (probe.x, probe.y))
+
     def step(self, obs):
         super(ProtossAgent, self).step(obs)
+
+        if obs.first():
+            #Self Position
+            player_y, player_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
+            xmean = player_x.mean()
+            ymean = player_y.mean()
+
+            if xmean <= 31 and ymean <= 31:
+                self.attack_coordinates = (49, 49)
+            else:
+                self.attack_coordinates = (12, 16) #X, Y
+
+        minerals = obs.observation.player.minerals
+        vespene = obs.observation.player.vespene
+
+        #Create Pylon in every oportunity (4 total)
+        Pylon = self.get_units_by_type(obs, units.Protoss.Pylon)
+        if len(Pylon) < 4 and minerals >= 100:
+            if self.can_do(obs, actions.FUNCTIONS.Build_Pylon_screen.id):
+                x = random.randint(0, 83)
+                y = random.randint(0, 83)
+                return actions.FUNCTIONS.Build_Pylon_screen("now", (x, y))
 
         return actions.FUNCTIONS.no_op()
         
